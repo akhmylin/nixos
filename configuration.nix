72c863f6e689cde612e1da1d9a26ac9655170f81
Andrew Akhmylin <akhmylin@gmail.com>
2020-06-11 00:58:31 +0700

    anki

--- configuration.nix
+++ configuration.nix
@@ -61,7 +61,7 @@
     environment.systemPackages = with pkgs; [
       polkit_gnome htop chromium electrum gajim git keepassxc stow tdesktop thunderbird tor-browser-bundle-bin
       transmission-gtk zathura unrar lm_sensors gimp gnome3.file-roller firefox libreoffice monero-gui mpv openvpn pciutils 
-      unzip zip yacreader 
+      unzip zip yacreader anki
     ];
     
     nixpkgs.config.allowUnfree = true;
